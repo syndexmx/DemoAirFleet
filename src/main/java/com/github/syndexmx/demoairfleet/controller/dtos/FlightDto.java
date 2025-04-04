bbec74da69068c0db74ae6f51a5f9f03d5cea8d8
Maxim Shapovalov <syndex@list.ru>
2025-04-04 10:27:23 +0300

    Swagger/Open-API DTOs fully annotated

--- src/main/java/com/github/syndexmx/demoairfleet/controller/dtos/FlightDto.java
+++ src/main/java/com/github/syndexmx/demoairfleet/controller/dtos/FlightDto.java
@@ -15,14 +15,31 @@ import lombok.NoArgsConstructor;
         description = "Полёт")
 public class FlightDto {
 
+    @Schema(name = "id", description = "Идентификатор", example = "17")
     private String id;
+
+    @Schema(name = "Name of flight", description = "Наименование маршрута", example = "Madrid to New-York")
     private String name;
+
+    @Schema(name = "Route identifier", description = "Идентификатор маршрута", example = "MH-48")
     private String route;
+
+    @Schema(name = "Call-sign", description = "Присвоенный позывной", example = "7486")
     private String callsign;
+
+    @Schema(name = "Number of passengers", description = "Количество пассажирова", example = "189")
     private Integer pax;
+
+    @Schema(name = "Date", description = "Дата полёта", example = "2025-03-19")
     private String date;
+
+    @Schema(name = "Aircraft", description = "Самолёт (борт)", example = "")
     private AircraftDto aircraft;
+
+    @Schema(name = "Captain", description = "Капитан", example = "[]")
     private PilotDto captain;
+
+    @Schema(name = "First officer", description = "Второй пилот", example = "[]")
     private PilotDto firstOfficer;
 
 }
